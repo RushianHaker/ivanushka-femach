0cfbde004f7b995bd10bb08be1d40bee6aef38ec
RushianHaker <ivanov2001m352g@mail.ru>
2018-02-01 11:13:33 +0300

    Initial Commit

---
+++ ivanov2001/ыек3.swift
@@ -0,0 +1,26 @@
+//
+//  ыек3.swift
+//  ivanov2001
+//
+//  Created by student on 01.02.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class str3: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+
